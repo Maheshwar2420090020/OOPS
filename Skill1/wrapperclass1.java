a53ce5f71209c1f63765e4b9821404e59313c921
Maheshwar2420090020 <2420090020@klh.edu.in>
2025-08-11 15:34:15 +0530

    Add files via upload

---
+++ Skill1/wrapperclass1.java
@@ -0,0 +1,13 @@
+package basic_concepts;
+
+public class wrapperclass1 {
+	public static void main(String args[])
+	{  
+	     
+	    int a=20;  
+	   Integer i=Integer.valueOf(a);  
+	   Integer j=a; 
+	   System.out.println(a+" "+i+" "+j);  
+	  }
+
+}
---
+++ Skill1/wrapperclass2.java
@@ -0,0 +1,13 @@
+package basic_concepts;
+
+public class wrapperclass2 {
+	public static void main(String args[])
+	 {    
+	       
+	    Integer a= 3;    
+	    int i=a.intValue();  
+	    int j=a;     
+	    System.out.println(a+" "+i+" "+j);    
+	   }
+
+}
